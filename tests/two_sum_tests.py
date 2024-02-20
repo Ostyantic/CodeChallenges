147e03c46593de674fb5d142b44bedd052e77091
Anthony Lopez <Antl21292@gmail.com>
2024-02-20 18:26:21 -0500

    test #3

--- tests/two_sum_tests.py
+++ tests/two_sum_tests.py
@@ -2,12 +2,21 @@ from code_challenges.two_sum import two_sum
 
 
 def test_two_sum_exists():
-    numbers = [1, 3, 5]
-    actual = two_sum(numbers, 8)
-    assert actual
+    two_sum([1, 2, 3], 3)
+
 
 def test_empty_list():
     empty_list = []
     actual = two_sum(empty_list, 8)
     expected = "empty list"
     assert actual == expected
+
+
+def test_target_8():
+    nums = [1, 3, 5]
+    target = 8
+
+    actual = two_sum(nums, target)
+    expected = [1, 2]
+
+    assert expected == actual
